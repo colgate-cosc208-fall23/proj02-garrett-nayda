f0c7f33d889ead806cc6ef7da61722f940bac0c8
Nayda Farnsworth <nfarnsworth@colgate.edu>
2024-02-04 15:19:55 -0500

    cleaned up some more code and started AI implementation

--- game.c
+++ game.c
@@ -10,6 +10,7 @@
 #include <ctype.h>
 
 #define PRACTICE_MODE 1
+#define AI_MODE 2
 
 struct coord { // struct that represents coordinate on the grid
     int x;
@@ -36,48 +37,38 @@ void practice_mode_instructions(){
     printf("   * if you hit all of the ships within your guess limit, you win. otherwise, you lose.\n\n");
 }
 
-void easy_AI_instructions(){
+void AI_instructions(){
     printf("easy AI player instructions: \n\n");
-    printf("empty rn\n");
+    printf("not implemented yet :'(\n");
 }
 
-void hard_AI_instructions(){
-    printf("hard AI player instructions: \n\n");
-    printf("empty rn\n");
-}
 int game_instructions() { // prints game instructions 
     printf("\nwelcome to Battleship! (press CTRL C to quit) \n\n");
     printf("select game mode (press enter/return to continue) \n");
     printf("   * practice mode: press 1\n");
-    printf("   * easy AI: press 2\n");
-    printf("   * hard AI: press 3\n");
-    printf("\n");
+    printf("   * AI: press 2\n\n");
 
     char game_choice;
-
     do {
         scanf(" %c", &game_choice);
-    } 
-    while (game_choice != '1' && game_choice != '2' && game_choice !='3');
+    } while (game_choice != '1' && game_choice != '2');
 
     printf("\n");
     if (game_choice == '1'){
         practice_mode_instructions();
         return 1;
     } else if (game_choice == '2'){
-        easy_AI_instructions();
+        AI_instructions();
         return 2;
-    } else if (game_choice == '3'){
-        hard_AI_instructions();
-        return 3;
-    }
-    
-    
+    } 
+
+    return -1;  
 }
 
 
 
-void print_grid(char **grid) { //takes a grid as parameter --> prints each of the 10 rows
+void print_grid(char **grid) { 
+    // given a grid, print_grid prints the grid
     printf(" 0123456789\n");
     for (int i = 0; i < 10; i++) {
         printf("%d%s\n", i, grid[i]);
@@ -87,51 +78,50 @@ void print_grid(char **grid) { //takes a grid as parameter --> prints each of th
 
 
 void free_grid(char **grid) {
+    // given a grid, free_grid frees all malloced space for a grid
     for (int i = 0; i < 10; i++) {
         free(grid[i]);
     }
     free(grid);
 }
 
-void free_ships(struct ship *shipArr) {
-    free(shipArr);
+void free_ships(struct ship *ship_arr) {
+    //given an array of ships, free_ships frees all malloced space used for ships
+    free(ship_arr);
 }
 
-int shipsLeft(struct ship *shipArr) {
+int ships_left(struct ship *ship_arr) {
+    //given an array of ships, ship_left returns an int corresponding to the number of ships that have not been sunk. 
     int downCt = 0;
     for (int i = 0; i < 5; i++) {
-        if (shipArr[i].shipDown == 1) {
+        if (ship_arr[i].shipDown == 1) {
             downCt++;
         }
     }
     return 5 - downCt;
 }
 
-int shipDown(struct ship *ship, struct ship *shipArr) {
+int ship_down(struct ship *ship, struct ship *ship_arr) {
     if (ship->length == ship->hits) {
         ship->shipDown = 1;
-        printf("You sunk the %s! %d ships remaining.\n", ship->name, shipsLeft(shipArr));
+        printf("You sunk the %s! %d ships remaining.\n", ship->name, ships_left(ship_arr));
         return 1;
     }
     return 0;
 }
 
-int game_over(struct ship *shipArr) {
-    int downCt = 0;
+int all_ships_down(struct ship *ship_arr) {
     for (int i = 0; i < 5; i++) {
-        if (shipArr[i].shipDown == 1) {
-            downCt++;
+        if (ship_arr[i].shipDown == 0) {
+            return 0; //false 
         }
     }
-    if (downCt == 5) {
-        printf("You win! You have successfully sunk all 5 ships!\n  ");
-        return 1;
-    }
-    return 0;
+    return 1;
 }
 
-struct ship *create_ships() {
-    struct ship *allShips = malloc(5 * sizeof(struct ship));
+struct ship *create_ships() { //creates an array of all 5 ships 
+
+    struct ship *allShips = malloc(5 * sizeof(struct ship)); //array of 5 ships 
 
     struct ship *patrolBoat = &allShips[0];
     patrolBoat->length = 2;
@@ -156,44 +146,44 @@ struct ship *create_ships() {
     return allShips;
 }
 
-int checkIfGridClear(char **grid, int x, int y, char dir, int len) {
+int check_if_grid_clear(char **grid, int row, int col, char dir, int len) {
     int lengthAfterStart = len - 1;
-    if (grid[x][y] == '-') {
+    if (grid[row][col] == '-') {
         if (dir == 'l') {
             for (int i = 1; i <= lengthAfterStart; i++) {
-                if ((y - i) < 0 || grid[x][y - i] != '-') {
+                if ((col - i) < 0 || grid[row][col - i] != '-') {
                     return 0;
                 }
             }
             for (int i = 0; i < len; i++) {
-                grid[x][y - i] = 'O';
+                grid[row][col - i] = 'O';
             }
         } else if (dir == 'r') {
             for (int i = 1; i <= lengthAfterStart; i++) {
-                if ((y + i) > 9 || grid[x][y + i] != '-') {
+                if ((col + i) > 9 || grid[row][col + i] != '-') {
                     return 0;
                 }
             }
             for (int i = 0; i < len; i++) {
-                grid[x][y + i] = 'O';
+                grid[row][col + i] = 'O';
             }
         } else if (dir == 'u') {
             for (int i = 1; i <= lengthAfterStart; i++) {
-                if ((x - i) < 0 || grid[x - i][y] != '-') {
+                if ((row - i) < 0 || grid[row - i][col] != '-') {
                     return 0;
                 }
             }
             for (int i = 0; i < len; i++) {
-                grid[x - i][y] = 'O';
+                grid[row - i][col] = 'O';
             }
         } else if (dir == 'd') {
             for (int i = 1; i <= lengthAfterStart; i++) {
-                if ((x + i) > 9 || grid[x + i][y] != '-') {
+                if ((row + i) > 9 || grid[row + i][col] != '-') {
                     return 0;
                 }
             }
             for (int i = 0; i < len; i++) {
-                grid[x + i][y] = 'O';
+                grid[row + i][col] = 'O';
             }
         }
         return 1;
@@ -201,66 +191,66 @@ int checkIfGridClear(char **grid, int x, int y, char dir, int len) {
     return 0;
 }
 
-void fill_ship_coords(struct ship *shipArr, char **grid) {
+void fill_ship_coords(struct ship *ship_arr, char **grid) {
     char directions[] = { 'u', 'd', 'l', 'r', '\0' }; // creates an array for the possible directions
     srand(time(NULL)); // seed the random number generator
 
     for (int i = 0; i < 5; i++) { // for each ship
-        shipArr[i].hits = 0;
-        shipArr[i].shipDown = 0;
-        int filled = 0; // checks if ship has been put on grid yet
+        ship_arr[i].hits = 0;
+        ship_arr[i].shipDown = 0; // false --> ship is not down
+        int filled = 0; // checks for potential collisions 
 
         while (filled == 0) {
-            int x = rand() % 10;
+            int x = rand() % 10; //generate a random x and y
             int y = rand() % 10;
-            char direction = directions[rand() % 4]; // random 0-3
+            char direction = directions[rand() % 4]; // choose a random direction
 
-            shipArr[i].coords.x = x; // Fills the coords array struct in each ship
-            shipArr[i].coords.y = y;
-            shipArr[i].direction = direction;
+            ship_arr[i].coords.x = x; 
+            ship_arr[i].coords.y = y;
+            ship_arr[i].direction = direction;
 
-            if (checkIfGridClear(grid, x, y, direction, shipArr[i].length) == 1) {
+            if (check_if_grid_clear(grid, x, y, direction, ship_arr[i].length) == 1) {
                 filled = 1;
             }
         }
     }
 }
 
-void hitWhichShip(int valX, int valY, struct ship *ships) {
+void hit_which_ship(int x, int y, struct ship *ships) {
     for (int i = 0; i < 5; i++) { // checking each ship
-        if (valX == ships[i].coords.x && valY == ships[i].coords.y) { // if it hits the starting coordinates, then automatically done
+        if (x == ships[i].coords.x && y == ships[i].coords.y) { // if it hits the starting coordinates, then automatically done
             ships[i].hits++;
-            shipDown(&ships[i], ships);
+            ship_down(&ships[i], ships);
             break;
         } else if (ships[i].direction == 'u') {
             for (int j = 1; j < ships[i].length; j++) {
-                if (valY == ships[i].coords.y && valX == (ships[i].coords.x - j)) {
+                if (x == ships[i].coords.y && y == (ships[i].coords.x - j)) {
                     ships[i].hits++;
-                    shipDown(&ships[i], ships);
+                    ship_down(&ships[i], ships);
                     break;
                 }
             }
         } else if (ships[i].direction == 'd') {
             for (int j = 1; j < ships[i].length; j++) {
-                if (valY == ships[i].coords.y && valX == (ships[i].coords.x + j)) {
+                if (y == ships[i].coords.y && x == (ships[i].coords.x + j)) {
                     ships[i].hits++;
-                    shipDown(&ships[i], ships);
+                    ship_down(&ships[i], ships);
                     break;
                 }
             }
         } else if (ships[i].direction == 'l') {
             for (int j = 1; j < ships[i].length; j++) {
-                if (valX == ships[i].coords.x && valY == (ships[i].coords.y - j)) {
+                if (x == ships[i].coords.x && y == (ships[i].coords.y - j)) {
                     ships[i].hits++;
-                    shipDown(&ships[i], ships);
+                    ship_down(&ships[i], ships);
                     break;
                 }
             }
         } else if (ships[i].direction == 'r') {
             for (int j = 1; j < ships[i].length; j++) {
-                if (valX == ships[i].coords.x && valY == (ships[i].coords.y + j)) {
+                if (x == ships[i].coords.x && y == (ships[i].coords.y + j)) {
                     ships[i].hits++;
-                    shipDown(&ships[i], ships);
+                    ship_down(&ships[i], ships);
                     break;
                 }
             }
@@ -268,101 +258,120 @@ void hitWhichShip(int valX, int valY, struct ship *ships) {
     }
 }
 
-void fill_player_guess(char **grid, struct ship *ships, char **playerGrid, int *misses,struct ship *shipArr, int *guess_limit) {
-    char x[3];
-    char y[3];
-    int valX;
-    int valY;
+void fill_player_guess(char **grid, struct ship *ships, char **playerGrid, int *misses,struct ship *ship_arr, int *guess_limit) {
+    int x;
+    int y;
 
-    printf("enter a row number (0-9): ");
-    fgets(x, 3, stdin);
-    valX = atoi(x);
+    printf("Enter a row number (0-9): ");
+    scanf("%d", &x);
 
-    printf("enter a column number (0-9): ");
-    fgets(y, 3, stdin);
-    valY = atoi(y);
+    printf("Enter a column number (0-9): ");
+    scanf("%d", &y);
 
-    if ((isdigit(x[0]) == 0 || isdigit(y[0]) == 0) || (valX < 0 || valX > 9) || (valY < 0 || valY > 9)) {
-        printf("invalid input! make sure you enter a number 0-9.\n");
-        // need to get inputs again. While loop until both are 0-9?
-    } else if (grid[valX][valY] == 'M' || grid[valX][valY] == 'H') {
-        printf("\ncoordinates already guessed! enter a new set of coordinates.\n");
-    } else if (grid[valX][valY] == 'O') {
-        grid[valX][valY] = 'H';
-        playerGrid[valX][valY] = 'H';
-        printf("\nHit at (%d,%d)!  marked with an H (Hit)!\n\n", valX, valY);
-        hitWhichShip(valX, valY, ships);
-        printf("\nyou have %d ships left to sink.\n\n",shipsLeft(shipArr));
-    } else if (grid[valX][valY] == '-') {
-        grid[valX][valY] = 'M';
-        playerGrid[valX][valY] = 'M';
-        printf("\nguess at row %d, column %d was not a hit. marked with an M (Miss).\n", valX, valY);
+    if ((x < 0 || x > 9) || (y < 0 || y > 9)) {
+        printf("\ninvalid input - enter a number 0-9.\n");
+    } else if (grid[x][y] != '-') {
+        printf("\ncoordinates already guessed - enter a new set of coordinates.\n");
+    } else if (grid[x][y] == 'O') {
+        grid[x][y] = 'H';
+        playerGrid[x][y] = 'H';
+        printf("\nhit at (%d,%d)! marked with an H (Hit)!\n\n", x, y);
+        hit_which_ship(x, y, ships);
+        printf("\nyou have %d ships left to sink.\n\n", ships_left(ship_arr));
+    } else if (grid[x][y] == '-') {
+        grid[x][y] = 'M';
+        playerGrid[x][y] = 'M';
+        printf("\nguess at row %d, column %d was not a hit. marked with an M (miss).\n", x, y);
         (*misses)++;
-        (*guess_limit)--;
-        printf("\nyou have missed %d attempt(s) so far. you have %d misses remaining. \n", *misses, *guess_limit);
-        printf("\nyou have %d ships left to sink.\n\n",shipsLeft(shipArr));
-        
+        printf("\nyou have missed %d attempt(s) so far. you have %d misses remaining.\n", *misses, (*guess_limit - *misses));
+        printf("\nyou have %d ships left to sink.\n\n", ships_left(ship_arr));
     }
 }
 
 char **grid_maker() { //mallocs and fills a 10x10 grid for graphics and data collection 
     char **grid = malloc(sizeof(char *) * 10); //malloc space for a 10 x 10 2D array
-
     for (int i = 0; i < 10; i++) { 
         grid[i] = malloc(sizeof(char) * 11); //create row
-
         for (int j = 0; j < 10; j++) {
             grid[i][j] = '-'; //fill with dashes
-        }
-
+        } 
         grid[i][10] = '\0'; //null terminate last index of each inner array 
     }
 
     return grid;
 }
 
+void practice_game(){
+    int misses = 0; // tracks the number of misses  
+    int guess_limit; //practice mode guess limit - user gets to choose how many tries they'd like to get 
+    printf("\nenter a guess limit: ");
+    scanf("%d", &guess_limit);
 
-void play_game() { //highest level function: runs the entirety of the game
-    int mode = game_instructions();
-    printf("%d\n", mode);
+    char **computer_grid = grid_maker(); // two grids - one tracks the data and another displays hits to the users 
+    char **player_visible_grid = grid_maker();
 
-    printf("press return to continue "); //works but look into this more 
-    int c; 
-    while ((c = getchar()) != '\n' && c != EOF);
-    getchar();
+    struct ship *all_ships = create_ships();
+    fill_ship_coords(all_ships, computer_grid); //look into this function more --- it seems pretty messy 
 
-    if (mode == PRACTICE_MODE){
-        int misses = 0; // tracks the number of misses  
+     while (all_ships_down(all_ships) == 0 && guess_limit != misses) {
+        fill_player_guess(computer_grid, all_ships, player_visible_grid, &misses, all_ships, &guess_limit);
+        print_grid(player_visible_grid);
+     }
+     if (guess_limit == misses){
+        printf("\nNo guesses remain! Game over.");
+     }
+     else {
+        printf("\nYou have successfully sunk all enemy ships! You win.");
+     }
 
-        int guess_limit; //practice mode guess limit - user gets to choose how many tries they'd like to get 
-        printf("\nenter a guess limit: ");
-        scanf("%d", &guess_limit);
-
-        char **computer_grid = grid_maker(); // two grids - one tracks the data and another displays hits to the users 
-        char **player_visible_grid = grid_maker();
-
-        struct ship *all_ships = create_ships();
-        fill_ship_coords(all_ships, computer_grid);
-
-        while (game_over(all_ships) == 0) {
-            fill_player_guess(computer_grid, all_ships, player_visible_grid, &misses, all_ships, &guess_limit);
-            print_grid(player_visible_grid);
-
-            if (misses >= guess_limit) {
-                printf("game over! you lose.\n");
-                break;
-        }
-    }
-
-
-
+    
     free_grid(player_visible_grid); //frees all malloced space 
     free_grid(computer_grid);
     free_ships(all_ships);
-    }
-   
 }
 
+void AI_game(){
+    printf("this is AI mode\n");
+
+    char **player_grid = grid_maker(); //player's personal grid
+    char **player_visible_grid = grid_maker(); // version of computer grid that player is allowed to see
+    char **computer_grid = grid_maker(); // computer's grid
+
+    struct ship *player_ships = create_ships();
+    struct  ship *enemy_ships = create_ships();
+
+    //create the AI grid
+    //use the random generator to fill in the AI grid
+
+    // let player start first
+
+    // guess - same as in practice mode
+
+    // then have the ai guess one
+    // must be within bounds, can not guess the same coordinates twice
+
+    // each time it checks if the ships are down. that's all that it checks.
+    // if ships are down, other person wins 
+}
+
+
+void play_game() { //highest level function: runs the entirety of the game
+    int mode = game_instructions();
+    printf("press return to continue "); 
+
+    int c; 
+    while ((c = getchar()) != '\n');
+    getchar();
+
+    if (mode == PRACTICE_MODE){ 
+       practice_game();
+    }
+
+    if (mode == AI_MODE){
+        AI_game();
+    } 
+   
+}
 
 int main() {
     play_game();
